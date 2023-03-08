b423923b30ce21b8b4ca6d895515f304d800c8be
unknown <sdlbsso@gmail.com>
2023-03-08 10:42:09 -0300

    intentando tabla

--- src/app/project/project.page.ts
+++ src/app/project/project.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-project',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectPage implements OnInit {
 
-  constructor() { }
+
+  public page = 1;
+  public resultsCount = 3;
+
+
+  public data: any;
+
+  constructor(private http: HttpClient) { }
+
 
   ngOnInit() {
+    this.loadData();
+  }
+
+  loadData(){
+    this.http.get(`https://randomuser.me/api?page=${this.page}&results=${this.resultsCount}`).subscribe(data=>{
+        console.log(data);
+        this.data = data;
+    })
   }
 
 }
