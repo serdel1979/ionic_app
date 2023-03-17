4b68556d65b895003335b098934c8e1489f94e6f
unknown <sdlbsso@gmail.com>
2023-03-17 12:09:29 -0300

    edicion de observaciones

--- src/app/load-observations/load-observations.page.ts
+++ src/app/load-observations/load-observations.page.ts
@@ -21,28 +21,28 @@ export class LoadObservationsPage {
   name: string = '';
   description: string = '';
 
-  public base64: string | unknown  = '';
-  public urlImgBase64 : string = BASE64;
+  public base64: string | unknown = '';
+  public urlImgBase64: string = BASE64;
 
   public observation: Observation = { id: uuId(), description: "", photos: [] };
 
-  public photo: TPhoto = { id: uuId(), description: "", photo: undefined, urlPhoto: "" };
+  public subObservation: TPhoto = { id: uuId(), description: "", photo: undefined, urlPhoto: "" };
   public photosBase64: PhotoBase64[] = [];
 
   public load: boolean = false;
   public src: string | ArrayBuffer | null = '';
   public imgBlobBase64: any = '';
-  
-  public renderOk : boolean = false;
+
+  public renderOk: boolean = false;
 
   public edition: boolean = false;
 
   myParameterObservation: any;
 
-  constructor(private modalCtrl: ModalController, private indexDbService:  IndexDBService, private navParams: NavParams) {
+  constructor(private modalCtrl: ModalController, private indexDbService: IndexDBService, private navParams: NavParams) {
   }
 
-  ionViewWillEnter() { 
+  ionViewWillEnter() {
     this.myParameterObservation = this.navParams.get('observation');
     this.edition = this.myParameterObservation = this.navParams.get('edition');
   }
@@ -61,9 +61,9 @@ export class LoadObservationsPage {
       let blob = new Blob([image.base64String], { type: 'image/png' });
       this.src = `${BASE64}${image.base64String}`;
       this.base64 = await this.convertBlobToBase64(blob);
-      this.photo.photo = blob;
-      this.photo.id = uuId();
-      this.photo.urlPhoto = this.src;
+      this.subObservation.photo = blob;
+      this.subObservation.id = uuId();
+      this.subObservation.urlPhoto = this.src;
       this.load = true;
     }
   }
@@ -72,20 +72,26 @@ export class LoadObservationsPage {
     //this.observation.photos.push(this.photo);
     // this.photo.urlPhoto = URL.createObjectURL(this.photo.photo!);
     // console.log(this.photo.urlPhoto);
-    this.convertBlobToBase64(this.photo.photo!).then(c=>{
-      this.photo.urlPhoto = `${BASE64}${c}`;
-      this.observation.photos.push(this.photo);
-      // this.photosBase64.push(
-      //   { 
-      //     id: uuId(), 
-      //     description: this.photo.description, 
-      //     photo: `${BASE64}${c}`,
-      //     render: true
-      //   }
-      // );
-      this.photo = { id: uuId(), description: "", photo: undefined, urlPhoto: ""};
-      this.load = false;
-    })
+    if (this.subObservation.photo) {
+      this.convertBlobToBase64(this.subObservation.photo!).then(c => {
+        this.subObservation.urlPhoto = `${BASE64}${c}`;
+        this.observation.photos.push(this.subObservation);
+        // this.photosBase64.push(
+        //   { 
+        //     id: uuId(), 
+        //     description: this.photo.description, 
+        //     photo: `${BASE64}${c}`,
+        //     render: true
+        //   }
+        // );
+        this.subObservation = { id: uuId(), description: "", photo: undefined, urlPhoto: "" };
+        this.load = false;
+      })
+    }else{
+      console.log(this.subObservation);
+      this.observation.photos.push(this.subObservation);
+      this.subObservation = { id: uuId(), description: "", photo: undefined, urlPhoto: "" };
+    }
   }
 
 
@@ -98,20 +104,20 @@ export class LoadObservationsPage {
     reader.readAsText(blob);
   });
 
- 
+
 
 
 
 
   confirm() {
     console.log(this.edition);
-    if(this.edition){
-      this.indexDbService.editObservation(this.observation).then(c=>console.log('editado ',c));
+    if (this.edition) {
+      this.indexDbService.editObservation(this.observation).then(c => console.log('editado ', c));
       this.edition = false;
-    }else{
-      this.indexDbService.addObservation(this.observation).then(c=>console.log('agregado ',c));
+    } else {
+      this.indexDbService.addObservation(this.observation).then(c => console.log('agregado ', c));
     }
-    
+
     return this.modalCtrl.dismiss(this.name, 'confirm');
   }
 
